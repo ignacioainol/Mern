37c5fc4c4ed8e9472a237aa45ef100b8127bff39
Ignacio Ainol <iggnaxios@gmail.com>
2022-12-22 13:14:53 -0300

    Create React App

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Title Project</a>
+      </header>
+      <main>
+        list items
+      </main>
+    </div>
+  );
+}
+
+export default App;
