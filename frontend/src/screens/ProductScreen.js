eec412af8427ea87e1f6f27d576636c8731b73c6
Ignacio Ainol <iggnaxios@gmail.com>
2022-12-24 15:05:50 -0300

    create react context

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-import { useEffect, useReducer } from "react";
+import { useContext, useEffect, useReducer } from "react";
 import { Badge, Button, Col, ListGroup, Row } from "react-bootstrap";
 import { Helmet } from "react-helmet-async";
 import { useParams } from "react-router-dom"
 import { LoadingBox } from "../components/LoadingBox";
 import { MessageBox } from "../components/MessageBox";
 import { Rating } from "../components/Rating";
+import { Store } from "../Store";
 import { getError } from "../utils";
 
 const reducer = (state, action) => {
@@ -46,7 +47,13 @@ export const ProductScreen = () => {
             // setProducts(data);
         };
         fetchData();
-    }, [slug])
+    }, [slug]);
+
+    const { state, dispatch: ctxDispatch } = useContext(Store);
+    const addToCartHandler = () => {
+        ctxDispatch({ type: 'CART_ADD_ITEM', payload: { ...product, quantity: 1 } })
+    }
+
     return (
         loading ? (<LoadingBox />) :
             error ? (<MessageBox variant="danger">{error}</MessageBox>) :
@@ -98,7 +105,7 @@ export const ProductScreen = () => {
                                 {countInStock > 0 && (
                                     <ListGroup.Item>
                                         <div className="d-grid">
-                                            <Button variant="primary">Add to Cart</Button>
+                                            <Button onClick={addToCartHandler} variant="primary">Add to Cart</Button>
                                         </div>
                                     </ListGroup.Item>
                                 )}
